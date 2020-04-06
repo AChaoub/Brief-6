4c46705733738bad78c0d943490fa367ebb38b85
AChaoub <57253129+AChaoub@users.noreply.github.com>
2020-04-06 02:39:20 +0100

    modif2

--- Ausy.js
+++ Ausy.js
@@ -31,7 +31,7 @@ app.post('/register', (req, res) => {
     var arrayOfObjects = JSON.parse(usersData)
     arrayOfObjects.push(utilisateur)
 
-    fs.writeFileSync('InfoUtilisateur.json', JSON.stringify(arrayOfObjects,null,2))
+    fs.writeFileSync('InfoUtilisateur.json', JSON.stringify(arrayOfObjects, null, 2))
 
     res.json('Vous avez inscrit avec succés');
 })
@@ -43,42 +43,42 @@ var data = fs.readFileSync('Questions.json');
 var array = JSON.parse(data);
 
 // Recuperation Service -----> ouvrir Fichier Service.
-var data1= fs.readFileSync('Service.json');
+var data1 = fs.readFileSync('Service.json');
 var array1 = JSON.parse(data1);
 
-app.get('/', (req, res) => {
+app.get('/service', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'public/Service.html'));
 })
 
 //Recuperer descp.
 app.get("/displayDes", displayDes);
 function displayDes(req, res) {
-  res.send(array1);
+    res.send(array1);
 }
 
 //Remplir liste service.
 app.get("/Remplir", RemplirListeService);
 function RemplirListeService(req, res) {
-  res.send(array1);
+    res.send(array1);
 }
 //ajouter des services a l'interface
 app.get("/AjouterService", AjouterServiceInterf);
 function AjouterServiceInterf(req, res) {
-  res.send(array1);
+    res.send(array1);
 }
-   
+
 // Cas de Methode GET
 app.get('/stocker', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'public/Service.html'))
 })
 
 // Methode POST -----> Reception donnees 
-app.post('/stocker', (req, res) => {    
+app.post('/stocker', (req, res) => {
     // Recuperer la description d'un service choisi
     var Combo = req.body.ConBox;
     var x;
-    for(var i = 0 ;i<data.length;i++){
-        if(Combo == i){
+    for (var i = 0; i < data.length; i++) {
+        if (Combo == i) {
             x = i;
             // des = data[i].description;
         }
@@ -86,15 +86,15 @@ app.post('/stocker', (req, res) => {
     console.log(x);
     //Ajout De Question
     array.push({
-        "id": array.length +1 ,
+        "id": array.length + 1,
         "nom": Combo,
         "description": req.body.textZone
-        
+
     });
     // Sauvgarde Dans un fichier JSON
-    var arrayJson = JSON.stringify(array,null,2);
-    fs.writeFile("Questions.json",arrayJson,"utf-8",function(err){
-        if(err )throw err
+    var arrayJson = JSON.stringify(array, null, 2);
+    fs.writeFile("Questions.json", arrayJson, "utf-8", function (err) {
+        if (err) throw err
         console.log("done");
     });
     // en cas de reussite;
@@ -104,18 +104,18 @@ app.post('/stocker', (req, res) => {
 
 
 //Sauvergarder un service dans Un JSOn
-app.post('/stockerService', (req, res) => {    
+app.post('/stockerService', (req, res) => {
     //Ajout De Question
     array1.push({
-        "id": array1.length +1 ,
+        "id": array1.length + 1,
         "nom": req.body.NomS,
         "description": req.body.DesS
-        
+
     });
     // Sauvgarde Dans un fichier JSON
-    var arrayJson1 = JSON.stringify(array1,null,2);
-    fs.writeFile("Service.json",arrayJson1,"utf-8",function(err){
-        if(err )throw err
+    var arrayJson1 = JSON.stringify(array1, null, 2);
+    fs.writeFile("Service.json", arrayJson1, "utf-8", function (err) {
+        if (err) throw err
         console.log("done");
     });
     // en cas de reussite;
